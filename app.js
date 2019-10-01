6c41988e57063935ea0215275f4f675d2388cca8
Ruslan Naeltok <ruslannaeltok@gmail.com>
2019-10-02 01:16:21 +0300

    gateway service

---
+++ app.js
@@ -0,0 +1,16 @@
+const Gateway = require('micromq/gateway');
+
+require('dotenv').config();
+
+const app = new Gateway({
+    microservices: ['users'],
+    rabbit: {
+        url: process.env.RABBIT_URL,
+    },
+});
+
+app.post(['/signup'], async (req, res) => {
+    await res.delegate('users');
+});
+
+app.listen(process.env.PORT);
